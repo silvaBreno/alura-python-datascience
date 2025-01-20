2f1a383a42b8325804c79a95512a104990869337
unknown <breno.desouza20@gmail.com>
2025-01-20 19:17:28 -0300

    estudos alura

--- python/rascunho.py
+++ python/rascunho.py
@@ -1,6 +1,6 @@
 def split_teste(s):
-    return s.split(" ").capitalize()
+    return s.upper().split(" ")
 
 x = input("escreva um nome: ")
 verificar = split_teste(x)
-print(verificar)
+print(verificar[0])
